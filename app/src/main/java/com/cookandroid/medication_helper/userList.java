207edad2f2e866e6ae51f791030542ce73f73c51
yuno122 <yuno122@naver.com>
2022-05-13 20:50:03 +0900

    update project

--- app/src/main/java/com/cookandroid/medication_helper/userList.java
+++ app/src/main/java/com/cookandroid/medication_helper/userList.java
@@ -1,4 +1,28 @@
 package com.cookandroid.medication_helper;
 
-public class userList {
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class userList extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.userinfo);
+
+        Button button1 = findViewById(R.id.btn1);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent myIntent = new Intent(userList.this, hompage.class);
+                startActivity(myIntent);
+                finish();
+            }
+        });
+
+    }
 }
--- app/src/main/java/com/cookandroid/medication_helper/userRegister.java
+++ app/src/main/java/com/cookandroid/medication_helper/userRegister.java
@@ -1,4 +1,28 @@
 package com.cookandroid.medication_helper;
 
-public class userRegister {
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class userRegister extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.userinfo);
+
+        Button button = findViewById(R.id.Btn);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent myIntent = new Intent(userRegister.this, hompage.class);
+                startActivity(myIntent);
+                finish();
+            }
+        });
+
+    }
 }
