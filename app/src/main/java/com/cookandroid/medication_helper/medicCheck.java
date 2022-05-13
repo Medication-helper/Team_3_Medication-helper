b069867405880620095e160611d8f437a29d34c8
BaekInHyeok <bih63879@gmail.com>
2022-05-13 23:23:26 +0900

    복약 등록, 복약 조회 부분 메인화면 복귀 버튼 java 코딩

--- app/src/main/java/com/cookandroid/medication_helper/medicCheck.java
+++ app/src/main/java/com/cookandroid/medication_helper/medicCheck.java
@@ -1,6 +1,9 @@
 package com.cookandroid.medication_helper;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -11,5 +14,34 @@ public class medicCheck extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.forbiddenlist);
         setTitle("Medication Helper");
+
+        Button combbacktoMain=findViewById(R.id.combtabclose);
+        Button pregbacktoMain=findViewById(R.id.pregtabclose);
+        Button agebacktoMain=findViewById(R.id.agetabclose);
+
+        combbacktoMain.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent combtoMain=new Intent(getApplicationContext(),MainActivity.class);
+                startActivity(combtoMain);
+            }
+        });
+
+        pregbacktoMain.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent pregtoMain=new Intent(getApplicationContext(),MainActivity.class);
+                startActivity(pregtoMain);
+            }
+        });
+
+        agebacktoMain.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent agetoMain=new Intent(getApplicationContext(),MainActivity.class);
+                startActivity(agetoMain);
+            }
+        });
+
     }
 }
--- app/src/main/java/com/cookandroid/medication_helper/medicRegister.java
+++ app/src/main/java/com/cookandroid/medication_helper/medicRegister.java
@@ -1,6 +1,9 @@
 package com.cookandroid.medication_helper;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -11,5 +14,16 @@ public class medicRegister extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.medicationregister);
         setTitle("Medication Helper");
+
+
+        Button registerButton=findViewById(R.id.medicregister);
+
+        registerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent regMedic=new Intent(getApplicationContext(),MainActivity.class);
+                startActivity(regMedic);
+            }
+        });
     }
 }
