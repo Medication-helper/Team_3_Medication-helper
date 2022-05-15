692a1a179db3f96c0d906a1d35c1571581642c8d
BaekInHyeok <bih63879@gmail.com>
2022-05-15 21:55:37 +0900

    복약 조회 부분 TabHost 동작 작업

--- app/src/main/java/com/cookandroid/medication_helper/medicCheck.java
+++ app/src/main/java/com/cookandroid/medication_helper/medicCheck.java
@@ -1,19 +1,38 @@
 package com.cookandroid.medication_helper;
 
+import android.app.TabActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TabHost;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class medicCheck extends AppCompatActivity {
+import com.google.android.material.tabs.TabLayout;
+
+@SuppressWarnings("deprecation")
+public class medicCheck extends TabActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.forbiddenlist);
         setTitle("Medication Helper");
+
+        TabHost tabHost=getTabHost();
+
+        TabHost.TabSpec tabSpecComb=tabHost.newTabSpec("COMB").setIndicator("병용 금기");
+        tabSpecComb.setContent(R.id.TABCOMB);
+        tabHost.addTab(tabSpecComb);
+
+        TabHost.TabSpec tabSpecPreg=tabHost.newTabSpec("PREG").setIndicator("임부 금기");
+        tabSpecPreg.setContent(R.id.TABPREG);
+        tabHost.addTab(tabSpecPreg);
+
+        TabHost.TabSpec tabSpecAge=tabHost.newTabSpec("AGE").setIndicator("연령 금기");
+        tabSpecAge.setContent(R.id.TABAGE);
+        tabHost.addTab(tabSpecAge);
 
         Button combbacktoMain=findViewById(R.id.combtabclose);
         Button pregbacktoMain=findViewById(R.id.pregtabclose);
