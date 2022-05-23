c9898f8bf575f8de695a391cec4f9feffd67579b
HyeonJongAn <103738972+HyeonJongAn@users.noreply.github.com>
2022-05-23 21:18:10 +0900

    SQLite 연동한 유저 DB 추가

---
+++ app/src/main/java/com/cookandroid/medication_helper/myDBHelper.java
@@ -0,0 +1,5 @@
+package com.cookandroid.medication_helper;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
--- app/src/main/java/com/cookandroid/medication_helper/userRegister.java
+++ app/src/main/java/com/cookandroid/medication_helper/userRegister.java
@@ -1,13 +1,32 @@
 package com.cookandroid.medication_helper;
 
+import android.app.DatePickerDialog;
+import android.content.Context;
+import android.widget.DatePicker;
 import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.util.Calendar;
+
 public class userRegister extends AppCompatActivity {
+
+    myDBHelper myHelper;
+    EditText E_Name;
+    RadioGroup RG;
+    RadioButton RB_Man, RB_Woman;
+    Button btnBirthChoose,  btnComplete, btnInit;
+    SQLiteDatabase sqlDB;
+
     @Override
     public void onBackPressed() {
         super.onBackPressed();
@@ -23,16 +42,84 @@ public class userRegister extends AppCompatActivity {
         setContentView(R.layout.userregister);
         setTitle("Medication Helper");
 
-        Button btnComplete = (Button) findViewById(R.id.BtnComplete);
-        Button btnBack_userRegister = (Button) findViewById(R.id.BtnBack_userRegister);
+        E_Name = (EditText) findViewById(R.id.E_name);
+        RG = (RadioGroup) findViewById(R.id.RG);
+        RB_Man = (RadioButton) findViewById(R.id.RB_man);
+        RB_Woman = (RadioButton) findViewById(R.id.RB_woman);
+        btnBirthChoose = (Button) findViewById(R.id.BtnBirthChoose);
+        btnComplete = (Button) findViewById(R.id.BtnComplete);
+        btnInit = (Button) findViewById(R.id.BtnInit);
 
-        btnBack_userRegister.setOnClickListener(new View.OnClickListener() {
+        Calendar calendar = Calendar.getInstance();
+        int mYear = calendar.get(Calendar.YEAR);
+        int mMonth = calendar.get(Calendar.MONTH);
+        int mDay = calendar.get(Calendar.DAY_OF_MONTH);
+
+        DatePickerDialog datePickerDialog = new DatePickerDialog(this, android.R.style.Theme_Holo_Light_Dialog_MinWidth, new DatePickerDialog.OnDateSetListener() {
             @Override
-            public void onClick(View v) {
-                Intent BackToMain = new Intent(userRegister.this, MainActivity.class);
-                startActivity(BackToMain);
-                finish();
+            public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                btnBirthChoose.setText(dayOfMonth+"/" + (month+1) + "/" + year);
+            }
+        }, mYear, mMonth, mDay);
+
+        btnBirthChoose.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (btnBirthChoose.isClickable()) {
+                    datePickerDialog.show();
+                }
             }
         });
+
+        myHelper = new myDBHelper(this);
+
+        btnInit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                sqlDB = myHelper.getWritableDatabase();
+                myHelper.onUpgrade(sqlDB, 1, 2);
+                sqlDB.close();
+            }
+        });
+
+        btnComplete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                sqlDB = myHelper.getWritableDatabase();
+                switch (RG.getCheckedRadioButtonId()) {
+                    case R.id.RB_man:
+                        sqlDB.execSQL("INSERT INTO userTBL VALUES ( '"
+                                + E_Name.getText().toString() + "', '"
+                                + btnBirthChoose.getText().toString() + "', 'MAN');");
+                        break;
+                    case R.id.RB_woman:
+                        sqlDB.execSQL("INSERT INTO userTBL VALUES ( '"
+                                + E_Name.getText().toString() + "' , '"
+                                + btnBirthChoose.getText().toString() + "', 'WOMAN');");
+                        break;
+                    default:
+                        Toast.makeText(getApplicationContext(), "성별이 선택되지 않았습니다.", Toast.LENGTH_SHORT).show();
+                        break;
+                }
+            }
+        });
+    }
+
+
+    public class myDBHelper extends SQLiteOpenHelper {
+        public myDBHelper(Context context) {
+            super(context, "userTBL", null, 1);
+        }
+
+        @Override
+        public void onCreate(SQLiteDatabase db) {
+            db.execSQL("CREATE TABLE userTBL ( uName CHAR(20) PRIMARY KEY, uBirth CHAR(20), uGender CHAR(5));");
+        }
+
+        @Override
+        public void onUpgrade(SQLiteDatabase db, int i, int i1) {
+            db.execSQL("DROP TABLE IF EXISTS userTBL");
+            onCreate(db);
+        }
     }
 }
