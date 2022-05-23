de84720088b37f0fba5bdd6df4121332dcdc7700
HyeonJongAn <103738972+HyeonJongAn@users.noreply.github.com>
2022-05-23 21:20:04 +0900

    SQLite 활용한 유저등록 기능 추가

--- app/src/main/java/com/cookandroid/medication_helper/myDBHelper.java
+++ app/src/main/java/com/cookandroid/medication_helper/myDBHelper.java
@@ -3,3 +3,20 @@ package com.cookandroid.medication_helper;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+
+public class myDBHelper extends SQLiteOpenHelper {
+    public myDBHelper(Context context) {
+        super(context, "userTBL", null, 1);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL("CREATE TABLE userTBL ( uName CHAR(20) PRIMARY KEY, uBirth CHAR(20), uGender CHAR(5));");
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int i, int i1) {
+        db.execSQL("DROP TABLE IF EXISTS userTBL");
+        onCreate(db);
+    }
+}
--- app/src/main/java/com/cookandroid/medication_helper/userRegister.java
+++ app/src/main/java/com/cookandroid/medication_helper/userRegister.java
@@ -104,22 +104,4 @@ public class userRegister extends AppCompatActivity {
             }
         });
     }
-
-
-    public class myDBHelper extends SQLiteOpenHelper {
-        public myDBHelper(Context context) {
-            super(context, "userTBL", null, 1);
-        }
-
-        @Override
-        public void onCreate(SQLiteDatabase db) {
-            db.execSQL("CREATE TABLE userTBL ( uName CHAR(20) PRIMARY KEY, uBirth CHAR(20), uGender CHAR(5));");
-        }
-
-        @Override
-        public void onUpgrade(SQLiteDatabase db, int i, int i1) {
-            db.execSQL("DROP TABLE IF EXISTS userTBL");
-            onCreate(db);
-        }
-    }
 }
