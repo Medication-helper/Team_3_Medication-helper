66309d9aba68da288beb9857fe2f6c58e4fb0f39
BaekInHyeok <bih63879@gmail.com>
2022-05-30 21:05:44 +0900

    DB 받아서 처방전 목록 출력 기능 구현

--- app/src/main/java/com/cookandroid/medication_helper/MedicineListActivity.java
+++ app/src/main/java/com/cookandroid/medication_helper/MedicineListActivity.java
@@ -2,10 +2,20 @@ package com.cookandroid.medication_helper;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class MedicineListActivity extends AppCompatActivity {
+    Button delBtn;
+    ListView medicationListView;
 
     @Override
     public void onBackPressed() {
@@ -22,5 +32,26 @@ public class MedicineListActivity extends AppCompatActivity {
         setContentView(R.layout.activity_medicinelist);
         setTitle("Medication Helper");
 
+        medicationListView=(ListView)findViewById(R.id.medicationlist);
+        delBtn=(Button)findViewById(R.id.btndelete);
+
+        ArrayList<String> medicineArraylist=new ArrayList<>();
+        String[] medicineArray;//DB에서 받아온 처방약 목록을 저장하는 String 배열
+
+        ArrayList<String> mediclist=new ArrayList<>(Arrays.asList(medicineArray));
+
+        ArrayAdapter<String> adapter=new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,mediclist);
+
+        medicationListView.setAdapter(adapter);
+
+        delBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                adapter.clear();
+                adapter.notifyDataSetChanged();
+                medicationListView.setAdapter(adapter);
+
+            }
+        });
     }
 }
