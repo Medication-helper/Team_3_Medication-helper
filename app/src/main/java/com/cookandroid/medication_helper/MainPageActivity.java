2ffc7cc6f24a49ca0eb3c02197d873d693df7df3
TA-PP <kgrewd@naver.com>
2022-06-12 02:58:01 +0900

    코드 이해를 위한 주석 추가

--- app/src/main/java/com/cookandroid/medication_helper/MainPageActivity.java
+++ app/src/main/java/com/cookandroid/medication_helper/MainPageActivity.java
@@ -15,15 +15,17 @@ import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainPageActivity extends AppCompatActivity {
 
+    //뒤로가기 누르면 앱종료시키는 함수
     @Override
     public void onBackPressed() {
-        //super.onBackPressed();
+        //다이어로그를 화면에 나타냄
         AlertDialog.Builder exitDialogBuilder = new AlertDialog.Builder(MainPageActivity.this);
         exitDialogBuilder
                 .setTitle("프로그램 종료")
                 .setMessage("종료하시겠습니까?")
                 .setCancelable(false)
                 .setPositiveButton("네",
+                        //네를 누르면 앱 종료
                         new DialogInterface.OnClickListener() {
                             @Override
                             public void onClick(DialogInterface dialogInterface, int i) {
@@ -32,6 +34,7 @@ public class MainPageActivity extends AppCompatActivity {
                                 finish();
                             }
                         })
+                //아니오 누르면 다이어로그를 종료
                 .setNegativeButton("아니오",
                         new DialogInterface.OnClickListener() {
                             @Override
@@ -39,9 +42,7 @@ public class MainPageActivity extends AppCompatActivity {
                                 dialogInterface.cancel();
                             }
                         });
-
         AlertDialog exitDialog = exitDialogBuilder.create();
-
         exitDialog.show();
     }
 
@@ -54,21 +55,23 @@ public class MainPageActivity extends AppCompatActivity {
         BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNav);
         Button btnMediReg = findViewById(R.id.btnMediReg);
         Button btnMediCheck = findViewById(R.id.btnMediCheck);
-
         bottomNavigationView.setSelectedItemId(R.id.homeNav);
+
+        //바텀네비게이션을 나타나게 해주는 함수
         bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
                 switch(item.getItemId()){
+                    //page버튼을 누르면 액티비티 화면을 전환시켜준다
                     case R.id.pageNav:
                         startActivity(new Intent(getApplicationContext(), WebActivity.class));
                         overridePendingTransition(0, 0);
                         finish();
                         return true;
-
+                    //현재 페이지에서 보여주는 액티비티
                     case R.id.homeNav:
                         return true;
-
+                    //user버튼을 누르면 액티비티 화면을 전환시켜준다
                     case R.id.userNav:
                         startActivity(new Intent(getApplicationContext(), MyPageActivity.class));
                         overridePendingTransition(0, 0);
@@ -78,6 +81,7 @@ public class MainPageActivity extends AppCompatActivity {
                 return false;
             }
         });
+        //현재 액티비티에서 MedicRegisterActivity로 넘겨주는 버튼
         btnMediReg.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -85,7 +89,7 @@ public class MainPageActivity extends AppCompatActivity {
                 startActivity(mediRegIntent);
             }
         });
-
+        //현재 액티비티에서 MedicCheckActivity로 넘겨주는 버튼
         btnMediCheck.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
