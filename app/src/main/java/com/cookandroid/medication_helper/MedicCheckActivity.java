3733939a4cb96f9d62a1c638ce407d5f35c5f1a4
yuno122 <yuno122@naver.com>
2022-06-15 20:13:14 +0900

    주석 추가

--- app/src/main/java/com/cookandroid/medication_helper/MedicCheckActivity.java
+++ app/src/main/java/com/cookandroid/medication_helper/MedicCheckActivity.java
@@ -10,12 +10,12 @@ import androidx.appcompat.app.AppCompatActivity;
 @SuppressWarnings("deprecation")
 public class MedicCheckActivity extends AppCompatActivity {
     @Override
-    public void onBackPressed() {
+    public void onBackPressed() {  // 하단의 뒤로가기 버튼을 눌렀을 시 동작
         super.onBackPressed();
-        Intent BackToMain = new Intent(MedicCheckActivity.this, MainPageActivity.class);
-        BackToMain.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-        startActivity(BackToMain);
-        finish();
+        Intent BackToMain = new Intent(MedicCheckActivity.this, MainPageActivity.class); // 메인화면으로 돌아가는 기능
+        BackToMain.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP); // 복약 정보 조회 페이지가 백그라운드에서 돌아가지 않도록 완전종료
+        startActivity(BackToMain); // 실행
+        finish(); // Progress 완전 종료
     }
 
     @Override
@@ -30,7 +30,7 @@ public class MedicCheckActivity extends AppCompatActivity {
         Button btnFBage = findViewById(R.id.btnFBage);
         Button btnMainPage= findViewById(R.id.btnback5);
 
-        btnMedilist.setOnClickListener(new View.OnClickListener() {
+        btnMedilist.setOnClickListener(new View.OnClickListener() { // 클릭 시 약 목록 조회 페이지로 이동
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(MedicCheckActivity.this, MedicineListActivity.class);
@@ -38,7 +38,7 @@ public class MedicCheckActivity extends AppCompatActivity {
                 finish();
             }
         });
-        btnFBcom.setOnClickListener(new View.OnClickListener() {
+        btnFBcom.setOnClickListener(new View.OnClickListener() { // 클릭 시 병용 금지 약물 조회 페이지로 이동
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(MedicCheckActivity.this, ComForbiddenListActivity.class);
@@ -46,7 +46,7 @@ public class MedicCheckActivity extends AppCompatActivity {
                 finish();
             }
         });
-        btnFBpreg.setOnClickListener(new View.OnClickListener() {
+        btnFBpreg.setOnClickListener(new View.OnClickListener() { // 클릭 시 임부 금지 약물 조회 페이지로 이동
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(MedicCheckActivity.this, PregnantForbiddenListActivity.class);
@@ -54,7 +54,7 @@ public class MedicCheckActivity extends AppCompatActivity {
                 finish();
             }
         });
-        btnFBage.setOnClickListener(new View.OnClickListener() {
+        btnFBage.setOnClickListener(new View.OnClickListener() { // 클릭 시 효능 중복 약물 조회 페이지로 이동
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(MedicCheckActivity.this, DuplicateListActivity.class);
@@ -63,7 +63,7 @@ public class MedicCheckActivity extends AppCompatActivity {
             }
         });
 
-        btnMainPage.setOnClickListener(new View.OnClickListener() {
+        btnMainPage.setOnClickListener(new View.OnClickListener() { // 클릭 시 메인 페이지로 이동
             @Override
             public void onClick(View view) {
                 Intent BackToMain = new Intent(MedicCheckActivity.this, MainPageActivity.class);
