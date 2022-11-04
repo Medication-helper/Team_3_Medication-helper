8f29ef9353ef31f424cf060c042a79754c3bc634
BaekInHyeok <bih63879@gmail.com>
2022-11-04 15:21:55 +0900

    앱 내부에 카메라 촬영 기능 구현

--- app/src/main/java/com/cookandroid/medication_helper/MedicRegisterActivity.java
+++ app/src/main/java/com/cookandroid/medication_helper/MedicRegisterActivity.java
@@ -63,6 +63,274 @@ import java.util.concurrent.ExecutionException;
 
 public class MedicRegisterActivity extends AppCompatActivity {
 
+    Bitmap image;
+    Bitmap bitmap;
+    Bitmap rotatedbitmap;
+
+    private TessBaseAPI mTess;
+    String datapath = "";
+
+    PreviewView previewView;
+    Button btnStartCamera;
+    Button btnCaptureCamera;
+    TextView textView;
+
+    ProcessCameraProvider processCameraProvider;
+    int lensFacing = CameraSelector.LENS_FACING_BACK;
+    ImageCapture imageCapture;
+
+    String OCRresult;
+
+    Button btnRegister;
+    private String imageFilepath;
+    static final int REQUEST_IMAGE_CAPTURE = 672;
+
+    String[] EdiCodearray;//EDI 코드 목록을 저장하는 배열
+    String[] medicList;//OpenAPI를 이용해 의약품 이름 목록을 저장하는 배열
+    String data;
+
+    UserData userData;
+    com.cookandroid.medication_helper.MedicDBHelper myHelper;
+    SQLiteDatabase sqlDB;
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+        setContentView(R.layout.activity_medicregister);
+
+        userData = (UserData) getApplicationContext();
+        myHelper = new com.cookandroid.medication_helper.MedicDBHelper(this);
+        previewView = (PreviewView) findViewById(R.id.previewView);
+        btnStartCamera = (Button) findViewById(R.id.btnCameraStart);
+        btnCaptureCamera = (Button) findViewById(R.id.btnPicture);
+        textView = (TextView) findViewById(R.id.OCRTextResult);
+
+        //언어 파일 경로 설정
+        datapath = getFilesDir() + "/tessaract/";
+
+        //언어 파일 존재 여부 확인
+        checkFile(new File(datapath + "tessdata/"), "eng");
+
+        String lang = "eng";
+
+        mTess = new TessBaseAPI();//TessBaseAPI 생성
+        mTess.init(datapath, lang);//초기화
+
+        //숫자만 인식해서 추출하도록 블랙리스트, 화이트리스트 설정
+        mTess.setVariable(TessBaseAPI.VAR_CHAR_BLACKLIST, ".,!?@#$%&*()<>_-+=/:;'\"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz");
+        mTess.setVariable(TessBaseAPI.VAR_CHAR_WHITELIST, "0123456789");
+
+        //카메라 촬영을 위한 동의 얻기
+        ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA}, 1);
+        try {
+            processCameraProvider = processCameraProvider.getInstance(this).get();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        //카메라 프리뷰 작동
+        btnStartCamera.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (ActivityCompat.checkSelfPermission(MedicRegisterActivity.this, Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED) {
+                    previewView.setVisibility(View.VISIBLE);
+                    textView.setVisibility(View.INVISIBLE);
+                    btnStartCamera.setVisibility(View.INVISIBLE);
+                    btnStartCamera.setEnabled(false);
+                    btnCaptureCamera.setVisibility(View.VISIBLE);
+                    btnCaptureCamera.setEnabled(true);
+
+                    bindPreview();
+                    bindImageCapture();
+
+                }
+            }
+        });
+
+        //카메라에 접근해 사진 찍는 버튼
+        btnCaptureCamera.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                imageCapture.takePicture(ContextCompat.getMainExecutor(MedicRegisterActivity.this),
+                        new ImageCapture.OnImageCapturedCallback() {
+                            @Override
+                            public void onCaptureSuccess(@NonNull ImageProxy image) {
+                                @SuppressLint("UnsafeExperimentalUsageError")
+                                Image mediaImage = image.getImage();
+                                bitmap = com.cookandroid.medication_helper.ImageUtil.mediaImageToBitmap(mediaImage);
+
+                                Log.d("MainActivity", Integer.toString(bitmap.getWidth())); //4128
+                                Log.d("MainActivity", Integer.toString(bitmap.getHeight())); //3096
+
+                                //imageView.setImageBitmap(bitmap);
+                                rotatedbitmap = com.cookandroid.medication_helper.ImageUtil.rotateBitmap(bitmap, image.getImageInfo().getRotationDegrees());
+
+                                Log.d("MainActivity", Integer.toString(rotatedbitmap.getWidth())); //3096
+                                Log.d("MainActivity", Integer.toString(rotatedbitmap.getHeight())); //4128
+                                Log.d("MainAtivity", Integer.toString(image.getImageInfo().getRotationDegrees()));
+                                //90 //0, 90, 180, 90 //이미지를 바르게 하기위해 시계 방향으로 회전해야할 각도
+
+                                processCameraProvider.unbindAll();//카메라 프리뷰 중단
+                                //pictureImage.setImageBitmap(rotatedBitmap);
+                                previewView.setVisibility(View.INVISIBLE);
+                                //pictureImage.setVisibility(View.VISIBLE);
+
+                                super.onCaptureSuccess(image);
+
+                                int height=rotatedbitmap.getHeight();
+                                int width=rotatedbitmap.getWidth();
+
+                                //AlertDialog에 사용할 비트맵 이미지의 사이즈를 가로세로 비율 맞춰 축
+                                Bitmap popupBitmap=Bitmap.createScaledBitmap(rotatedbitmap,1000,height/(width/1000),true);
+
+                                //카메라 바인딩 사용중단
+                                processCameraProvider.unbindAll();
+
+                                ImageView capturedimage = new ImageView(MedicRegisterActivity.this);
+                                capturedimage.setImageBitmap(popupBitmap);
+
+                                //사진 촬영 결과를 AlertDialog로 띄워 사용 여부를 선택한다
+                                AlertDialog.Builder captureComplete=new AlertDialog.Builder(MedicRegisterActivity.this)
+                                        .setTitle("촬영 결과")
+                                        .setMessage("이 사진을 사용할까요?")
+                                        .setView(capturedimage)
+                                        //사용을 선택할 경우 OCR 실행
+                                        .setPositiveButton("사용", new DialogInterface.OnClickListener() {
+                                            @Override
+                                            public void onClick(DialogInterface dialog, int which) {
+                                                String OCRresult=null;
+                                                mTess.setImage(rotatedbitmap);
+                                                OCRresult=mTess.getUTF8Text();
+
+                                                textView.setText(OCRresult);
+                                            }
+                                        })
+                                        //재촬영을 선택할 경우 bitmap에 저장된 비트맵 파일을 지우고 다시 카메라 프리뷰를 바인딩함
+                                        .setNegativeButton("재촬영", new DialogInterface.OnClickListener() {
+                                            @Override
+                                            public void onClick(DialogInterface dialog, int which) {
+                                                bitmap=null;
+                                                bindPreview();
+                                                bindImageCapture();
+                                                textView.setVisibility(View.INVISIBLE);
+                                                //pictureImage.setVisibility(View.INVISIBLE);
+                                                previewView.setVisibility(View.VISIBLE);
+                                            }
+                                        });
+
+                                captureComplete.setCancelable(false);
+
+                                captureComplete.create().show();
+                            }
+                        });
+            }
+        });
+
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNav);
+        //Button btnPill = findViewById(R.id.pillbtn);
+        //Button btnJar = findViewById(R.id.jarbtn);
+        bottomNavigationView.setSelectedItemId(R.id.cameraNav);
+
+        //바텀네비게이션을 나타나게 해주는 함수
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch(item.getItemId()){
+                    //home버튼을 누르면 액티비티 화면을 전환시켜준다
+                    case R.id.homeNav:
+                        startActivity(new Intent(getApplicationContext(), com.cookandroid.medication_helper.MainPageActivity.class));
+                        overridePendingTransition(0, 0);
+                        finish();
+                        return true;
+                    //현재 화면에서 보여주는 액티비티
+                    case R.id.cameraNav:
+                        return true;
+                    //article 버튼을 누르면 액티비티 화면을 전환시켜준다
+                    case R.id.articleNav:
+                        startActivity(new Intent(getApplicationContext(), MedicineListActivity.class));
+                        overridePendingTransition(0, 0);
+                        finish();
+                        return true;
+                    //user 버튼을 누르면 액티비티 화면을 전환시켜준다
+                    case R.id.userNav:
+                        startActivity(new Intent(getApplicationContext(), com.cookandroid.medication_helper.MyPageActivity.class));
+                        overridePendingTransition(0, 0);
+                        finish();
+                        return true;
+                }
+                return false;
+            }
+        });
+    }
+
+    void bindPreview(){
+        previewView.setScaleType(PreviewView.ScaleType.FIT_CENTER);
+        CameraSelector cameraSelector = new CameraSelector.Builder()
+                .requireLensFacing(lensFacing)
+                .build();
+
+        Preview preview = new Preview.Builder()
+                .setTargetAspectRatio(AspectRatio.RATIO_4_3)
+                .build();
+        preview.setSurfaceProvider(previewView.getSurfaceProvider());
+        processCameraProvider.bindToLifecycle(this,cameraSelector,preview);
+    }
+
+    void bindImageCapture(){
+        CameraSelector cameraSelector=new CameraSelector.Builder()
+                .requireLensFacing(lensFacing)
+                .build();
+        imageCapture=new ImageCapture.Builder()
+                .build();
+
+        processCameraProvider.bindToLifecycle(this,cameraSelector,imageCapture);
+    }
+
+    //스마트폰에 사진 파일 복사
+    private void copyFiles(String lang){
+        try{
+            String filepath=datapath+"/tessdata/"+lang+".traineddata";
+
+            AssetManager assetManager=getAssets();
+
+            InputStream inputStream=assetManager.open("tessdata/"+lang+".traineddata");
+            OutputStream outputStream=new FileOutputStream(filepath);
+
+            byte[] buffer=new byte[1024];
+            int read;
+
+            while ((read=inputStream.read(buffer))!=-1){
+                outputStream.write(buffer,0,read);
+            }
+            outputStream.flush();
+            outputStream.close();
+            inputStream.close();
+        }catch (FileNotFoundException e){
+            e.printStackTrace();
+        }catch(IOException e){
+            e.printStackTrace();
+        }
+    }
+
+    //스마트폰에 파일이 있는 지 확인
+    private void checkFile(File dir, String lang){
+        if(!dir.exists()&&dir.mkdirs()){
+            copyFiles(lang);
+        }
+        if(dir.exists()){
+            String datafilepath=datapath+"/tessdata/"+lang+".traineddata";
+            File datafile=new File(datafilepath);
+            if(!datafile.exists()){
+                copyFiles(lang);
+            }
+        }
+    }
+
+
+    /*
     Bitmap image;//사용되는 이미지
     private TessBaseAPI mTess;//Tess API Reference
     String datapath="";//언어데이터가 있는 경로
@@ -87,13 +355,18 @@ public class MedicRegisterActivity extends AppCompatActivity {
     String[] medicList;//OpenAPI를 이용해 받아온 의약품 이름 목록을 저장하는 배열
 
     String data;
+     */
 
     /* 의약품 DB를 사용하기 위한 변수들 */
+    /*
     UserData userData;
     MedicDBHelper myHelper;
     SQLiteDatabase sqlDB;
 
+     */
+
     /*스마트폰의 뒤로가기 버튼에 대한 뒤로가기 동작 구현*/
+    /*
     @Override
     public void onBackPressed() {
         super.onBackPressed();
@@ -198,7 +471,10 @@ public class MedicRegisterActivity extends AppCompatActivity {
                                 runOnUiThread(new Runnable() {
                                     @Override
                                     public void run() {
+
+     */
                                         /* 읽어들인 약품을 의약품 DB에 저장 */
+    /*
                                         int count = cursor.getCount() + 1;
                                         for (int i = 0; i < medicList.length; i++) {
                                             sqlDB.execSQL("INSERT INTO medicTBL VALUES ("
@@ -395,4 +671,6 @@ public class MedicRegisterActivity extends AppCompatActivity {
             }
         }
     }
+
+     */
 }
