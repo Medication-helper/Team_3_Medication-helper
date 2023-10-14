e5579173124ed8e6fa1fc5ccacacb7841a7e5b08
BaekInHyeok <bih63879@gmail.com>
2023-10-15 02:47:18 +0900

    디자인 수정 2차

--- Team_3_Medication-helper/app/src/main/java/com/cookandroid/medication_helper/SideEffectDetailActivity.java
+++ Team_3_Medication-helper/app/src/main/java/com/cookandroid/medication_helper/SideEffectDetailActivity.java
@@ -3,12 +3,15 @@ package com.cookandroid.medication_helper;
 import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.firebase.database.DataSnapshot;
@@ -20,6 +23,7 @@ import com.squareup.picasso.Picasso;
 
 public class SideEffectDetailActivity extends AppCompatActivity {
     EditText selectedmName, selectedcomponent, selectedmEffect, selectedsEffect, selectedpForbid;
+    Button btnOk;
 
     @Override
     public void onBackPressed() {
@@ -35,24 +39,29 @@ public class SideEffectDetailActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
         setContentView(R.layout.activity_sidedetail);
+        getSupportActionBar().setDisplayShowTitleEnabled(false); // 기본 타이틀 사용 안함
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.hide();
 
         Intent selectedMedicine = getIntent();
         String medicine = selectedMedicine.getStringExtra("selectedMedicine");
 
         selectedmName = (EditText) findViewById(R.id.SelectedmName_S);
-        selectedcomponent = (EditText) findViewById(R.id.Selectedcomponent);
+        //selectedcomponent = (EditText) findViewById(R.id.Selectedcomponent);
         selectedmEffect = (EditText) findViewById(R.id.SelectedmEffect_S);
         selectedsEffect = (EditText) findViewById(R.id.SelectedsEffect);
         selectedpForbid = (EditText) findViewById(R.id.SelectedpForbid);
 
+        btnOk=findViewById(R.id.btnOk);
+
         selectedmName.setEnabled(false);
-        selectedcomponent.setEnabled(false);
+        //selectedcomponent.setEnabled(false);
         selectedmEffect.setEnabled(false);
         selectedsEffect.setEnabled(false);
         selectedpForbid.setEnabled(false);
 
         selectedmName.setBackground(null);
-        selectedcomponent.setBackground(null);
+        //selectedcomponent.setBackground(null);
         selectedmEffect.setBackground(null);
         selectedsEffect.setBackground(null);
         selectedpForbid.setBackground(null);
@@ -64,8 +73,8 @@ public class SideEffectDetailActivity extends AppCompatActivity {
             @Override
             public void onDataChange(@NonNull DataSnapshot snapshot) {
                 if (snapshot.exists()) {
-                    if (snapshot.child("cName").exists())
-                        selectedcomponent.setText(snapshot.child("cName").getValue(String.class));
+//                    if (snapshot.child("cName").exists())
+//                        selectedcomponent.setText(snapshot.child("cName").getValue(String.class));
                     if (snapshot.child("mEffect").exists())
                         selectedmEffect.setText(snapshot.child("mEffect").getValue(String.class));
                     if (snapshot.child("cForbid").exists())
@@ -78,6 +87,16 @@ public class SideEffectDetailActivity extends AppCompatActivity {
             @Override
             public void onCancelled(@NonNull DatabaseError error) {
                 Toast.makeText(getApplicationContext(), "알 수 없는 에러입니다.", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        btnOk.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(SideEffectDetailActivity.this, SideEffectListActivity.class);
+                intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                startActivity(intent); // 실행
+                finish(); // Progress 완전 종료
             }
         });
     }
